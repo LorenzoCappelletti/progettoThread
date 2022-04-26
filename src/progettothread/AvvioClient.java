c216293b7e098f21799a2935cee19bdb6d869db2
lorenzo <lorycappe2003@gmail.com>
2022-04-26 17:31:02 +0200

    commento iniziale

---
+++ src/progettothread/AvvioClient.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progettothread;
+
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+/**
+ *
+ * @author loren
+ */
+public class AvvioClient 
+{
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            Client cli = new Client(InetAddress.getLocalHost(), 2000);
+        } 
+        catch (UnknownHostException ex) 
+        {
+            Logger.getLogger(AvvioClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+    }
+           
+}
---
+++ src/progettothread/AvvioServer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progettothread;
+
+/**
+ *
+ * @author loren
+ */
+public class AvvioServer 
+{
+    public static void main(String[] args) 
+    {
+        Server srv = new Server(2000); // crea un istanza della classe server 
+        srv.ascolto();
+        srv.scrivi("Benvenuto client"); 
+    }
+}
